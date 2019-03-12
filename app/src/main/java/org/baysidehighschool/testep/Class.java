9c72c7250a679d1cb7832001bbc58da7c8471a11
sli7166 <zchen7864@baysidehighschool.org>
2019-03-12 08:36:03 -0400

    second

---
+++ app/src/main/java/org/baysidehighschool/testep/Class.java
@@ -0,0 +1,13 @@
+package org.baysidehighschool.testep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Class extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class);
+    }
+}
