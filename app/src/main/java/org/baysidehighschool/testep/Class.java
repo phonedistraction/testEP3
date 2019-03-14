df74255a6a0f77634623e38a3e4f46d58fe87820
phonedistraction <zchen7864@baysidehighschool.org>
2019-03-14 08:28:55 -0400

    third

--- app/src/main/java/org/baysidehighschool/testep/Class.java
+++ app/src/main/java/org/baysidehighschool/testep/Class.java
@@ -2,6 +2,10 @@ package org.baysidehighschool.testep;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class Class extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class Class extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class);
+
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+
+        TextView textView = findViewById(R.id.editText);
+        textView.setText(currentDate);
     }
 }
