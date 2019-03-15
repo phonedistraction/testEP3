c46f32b2f1d46676260cc4fad1c502ccf5f37658
phonedistraction <zchen7864@baysidehighschool.org>
2019-03-15 08:33:15 -0400

    fourth

--- app/src/main/java/org/baysidehighschool/testep/profile.java
+++ app/src/main/java/org/baysidehighschool/testep/profile.java
@@ -2,12 +2,35 @@ package org.baysidehighschool.testep;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class profile extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class profile extends AppCompatActivity
+{
+    private TextView studentName, studentID;
+    private CircleImageView profileImage;
+
+    private DatabaseReference profileUser;
+    private FirebaseAuth mAuth;
+
+    private String currentUserId;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+
+        studentName = (TextView) findViewById(R.id.student);
+        studentID = (TextView) findViewById(R.id.id);
+        profileImage = (CircleImageView) findViewById(R.id.)
+
     }
 }
