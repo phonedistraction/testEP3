bee3755c71a2c761676a4a6805210afb3b52b655
sli7166 <zchen7864@baysidehighschool.org>
2019-04-05 08:13:59 -0400

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	app/src/main/java/org/baysidehighschool/testep/Class.java
    #	app/src/main/java/org/baysidehighschool/testep/profile.java
    #	app/src/main/res/layout/activity_class.xml

--- app/src/main/java/org/baysidehighschool/testep/SendMessage.java
+++ app/src/main/java/org/baysidehighschool/testep/SendMessage.java
@@ -2,12 +2,38 @@ package org.baysidehighschool.testep;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.security.PrivateKey;
 
 public class SendMessage extends AppCompatActivity {
+
+
+    //  private Firebase mRef;
+    private Button mSendData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_send_message);
+        setContentView(R.layout.activity_main);
+
+        //   Firebsae.setAndroidContext(this);
+        // mRef = new Firebase("https://my-projects-dcffa.firebaseio.com/");
+        mSendData =(Button) findViewById(R.id.sendData);
+
+        mSendData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("Name");
+
+                myRef.setValue("Nikmal");
+            }
+        });
+
     }
 }
